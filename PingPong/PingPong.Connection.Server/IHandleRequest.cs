a3a9f41ad88a73b447e223ebca9608600794ab18
unknown <yardenpikmans2003@gmail.com>
2022-01-19 16:30:55 +0200

    Create Handle request interface

---
+++ PingPong/PingPong.Connection.Server/IHandleRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingPong.Connection.Server
+{
+    public interface IHandleRequest<T, K>
+    {
+        K HandleRequest(T request);
+    }
+}
