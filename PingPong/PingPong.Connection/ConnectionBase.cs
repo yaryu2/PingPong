b2bf62f3ba0a99c6b992af008cf7b9bd7e13fad6
unknown <yardenpikmans2003@gmail.com>
2022-01-19 16:58:08 +0200

    Finish abstraction Connection

---
+++ PingPong/PingPong.Connection/ConnectionBase.cs
@@ -0,0 +1,13 @@
+﻿using PingPong.Connection.Abstract;
+using PingPong.Connection.Socket;
+
+namespace PingPong.Connection
+{
+    public abstract class ConnectionBase<TType, TWrite, TRead>
+    {
+        protected IHandleRequest<TRead> _handleRequest;
+        protected SocketBase<TType, TWrite, TRead> _socket;
+
+        public abstract void Run(string ip, int port);
+    }
+}
