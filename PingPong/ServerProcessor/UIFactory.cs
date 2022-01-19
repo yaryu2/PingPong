8fe3816c663de534611841f6ba43ba926328fecf
unknown <yardenpikmans2003@gmail.com>
2022-01-19 20:15:55 +0200

    Create ServerImplementation

---
+++ PingPong/ServerProcessor/UIFactory.cs
@@ -0,0 +1,18 @@
+﻿using PingPong.UI;
+using PingPong.UIImplementation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerProcessor
+{
+    public class UIFactory
+    {
+        public UIBase CreateUI()
+        {
+            UIBase ui = new ConsoleImplementation();
+        }
+    }
+}
