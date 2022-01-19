252a10c579c66d1e2387f5db3cad52a5fe266b9f
unknown <yardenpikmans2003@gmail.com>
2022-01-19 15:53:27 +0200

    Create implementation for reader

---
+++ PingPong/PingPong.UIImplementation/Reader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PingPong.UI.Abstraction;
+
+namespace PingPong.UIImplementation
+{
+    public class Reader : IReader<string>
+    {
+        public string Read()
+        {
+            return Console.ReadLine();
+        }
+    }
+}
