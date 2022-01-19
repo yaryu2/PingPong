0703ccad022c7a45fb6412fa056ecdee1a9ae72b
unknown <yardenpikmans2003@gmail.com>
2022-01-19 16:51:21 +0200

    Finish socket abstraction

--- PingPong/PingPong.Connection/Socket/SocketBase.cs
+++ PingPong/PingPong.Connection/Socket/SocketBase.cs
@@ -3,10 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PingPong.Connection.Socket.Abstraction;
 
 namespace PingPong.Connection.Socket
 {
-    public class ConnectionBase
+    public abstract class SocketBase<TType, TWrite, TRead>
     {
+        protected IWrite<TWrite> _write;
+        protected IReader<TRead> _reader;
+        protected IConnection<TType> _connection;
+
+        public virtual TType CreateConnection(string ip, int port)
+        {
+            return _connection.CreateConnection(ip, port);
+        }
+        public virtual TRead Read()
+        {
+            return _reader.Read();
+        }
+        public virtual void Write(TWrite value)
+        {
+            _write.Write(value);
+        }
     }
 }
