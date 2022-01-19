8ee85581e3dba9c003090ddadcbce665cad70855
unknown <yardenpikmans2003@gmail.com>
2022-01-19 14:47:50 +0200

    Initial the project

---
+++ PingPong/PingPong.Common/Program.cs
@@ -0,0 +1,20 @@
+﻿using log4net;
+using log4net.Config;
+using System.Configuration;
+using System.Reflection;
+
+
+namespace PingPong.Common
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Check the path of FileInfo
+            XmlConfigurator.Configure(new FileInfo(@"..\..\..\PingPong.Common.pdb"));
+            ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+            var value = ConfigurationManager.AppSettings[""];
+        }
+    }
+}
