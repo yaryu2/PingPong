b482bdfd7abfda9acec76d7f59c0436cc8050f74
unknown <yardenpikmans2003@gmail.com>
2022-01-19 15:24:14 +0200

    Create ui project

---
+++ PingPong/PingPong.UI/Program.cs
@@ -0,0 +1,20 @@
+﻿using log4net;
+using log4net.Config;
+using System.Configuration;
+using System.Reflection;
+
+
+namespace PingPong.UI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Check the path of FileInfo
+            XmlConfigurator.Configure(new FileInfo(@"..\..\..\PingPong.UI.pdb"));
+            ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+            var value = ConfigurationManager.AppSettings[""];
+        }
+    }
+}
